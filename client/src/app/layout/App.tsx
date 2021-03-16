747da4fd44ce457923c20b852c47be25c3ce1abd
Velin Arbov <48315855+VelinArbov@users.noreply.github.com>
2021-03-16 16:48:10 +0200

    Some Update

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,96 +1,29 @@
-import React, { useState, useEffect} from "react";
-import "./style.css";
-import { Container } from "semantic-ui-react";
-import { Activity } from "./models/Activity";
-
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import NavBar from "../../features/nav/NavBar";
-import { v4 as uuid } from "uuid";
-import agent from "../api/agent";
-import LoadingComponent from "./LoadingComponent";
-import { useStore } from "../stores/store";
+import React, { useEffect } from 'react';
+import { Container } from 'semantic-ui-react';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
+import NavBar from '../../features/nav/NavBar';
 
 function App() {
-  const { activityStore } = useStore();
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<
-    Activity | undefined
-  >(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const [submitting, setSubmitting] = useState(false);
+  const {activityStore} = useStore();
 
   useEffect(() => {
-   activityStore.loadActivities();
-  }, [activityStore]);
+    activityStore.loadActivities();
+  }, [activityStore])
 
-  function handleSelectActivity(id: string) {
-    setSelectedActivity(activities.find((x) => x.id === id));
-  }
-
-  function handleCancelSelectActivity() {
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?: string) {
-    id ? handleSelectActivity(id) : handleCancelSelectActivity();
-    setEditMode(true);
-  }
-
-  function handleFormClose() {
-    setEditMode(false);
-  }
-
-  function handleCreateOrEditActivity(activity: Activity) {
-    setSubmitting(true);
-    if (activity.id) {
-      agent.Activities.update(activity).then(() => {
-        setActivities([
-          ...activities.filter((x) => x.id !== activity.id),
-          activity,
-        ]);
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      });
-    } else {
-      activity.id = uuid();
-      agent.Activities.create(activity).then(() => {
-        setActivities([...activities, activity]);
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      });
-    }
-  }
-
-  function handleDeleteActivity(id: string) {
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter((x) => x.id !== id)]);
-      setSubmitting(false);
-    });
-  }
-  if (activityStore.loadingInitial) return <LoadingComponent content="Loading app..." />;
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
   return (
     <>
-      <NavBar openForm={handleFormOpen} />
-      <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard
-          activities={activityStore.activities}
-          selectedActivity={selectedActivity}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          editMode={editMode}
-          openForm={handleFormOpen}
-          closeForm={handleFormClose}
-          createOrEdit={handleCreateOrEditActivity}
-          deleteActivity={handleDeleteActivity}
-          submitting={submitting}
-        />
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard />
       </Container>
+
     </>
   );
 }
 
-export default App;
+export default observer(App);
--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,63 +1,26 @@
-import React from "react";
-import { Grid } from "semantic-ui-react";
-import { Activity } from "../../../app/layout/models/Activity";
+import { observer } from 'mobx-react-lite';
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import { useStore } from '../../../app/stores/store';
+import ActivityList from './ActivityList';
+import ActivityDetails from './details/ActivityDetails';
+import ActivityForm from './form/ActivityForm';
 
-import ActivityList from "./ActivityList";
-import ActivityDetails from "./details/ActivityDetails";
-import ActivityForm from "./form/ActivityForm";
+export default observer(function ActivityDashboard() {
 
-interface Props {
-  activities: Activity[];
-  selectedActivity: Activity | undefined;
-  selectActivity: (id: string) => void;
-  cancelSelectActivity: () => void;
-  editMode: boolean;
-  openForm: (id: string) => void;
-  closeForm: () => void;
-  createOrEdit: (activity: Activity) => void;
-  deleteActivity: (id: string) => void;
-  submitting: boolean;
-}
-
-export default function ActivityDashboard({
-  activities,
-  selectedActivity,
-  deleteActivity,
-  selectActivity,
-  cancelSelectActivity,
-  editMode,
-  openForm,
-  closeForm,
-  createOrEdit,
-  submitting
-}: Props) {
-  return (
-    <Grid>
-      <Grid.Column width="10">
-        <ActivityList
-          activities={activities}
-          selectActivity={selectActivity}
-          deleteActivity={deleteActivity}
-          submitting = {submitting}
-        />
-      </Grid.Column>
-      <Grid.Column width="6">
-        {selectedActivity && !editMode && (
-          <ActivityDetails
-            activity={selectedActivity}
-            cancelSelectActivity={cancelSelectActivity}
-            openForm={openForm}
-          />
-        )}
-        {editMode && (
-          <ActivityForm
-            submitting ={submitting}
-            closeForm={closeForm}
-            activity={selectedActivity}
-            createOrEdit={createOrEdit}
-          />
-        )}
-      </Grid.Column>
-    </Grid>
-  );
-}
+    const {activityStore} = useStore();
+    const {selectedActivity, editMode} = activityStore;
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity && !editMode &&
+                <ActivityDetails />}
+                {editMode &&
+                <ActivityForm />}
+            </Grid.Column>
+        </Grid>
+    )
+})
